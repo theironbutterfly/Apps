5591336f2724a3fd138bb53a782e7b63e637de63
Apoorva Tare <apoorvatare@gmail.com>
2013-09-14 19:20:50 -0700

    adding the skeleton of rails app

---
+++ scrum-int/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrumInt::Application.config.secret_token = '06658b52657a4c6a667e037f99f072e7e37093601a893f427d1e5cea286eea8c0b769f51dac4f0ef6c32b2e7f45db297bb5ad6a5a61cc3120f08ab0df32a774a'
