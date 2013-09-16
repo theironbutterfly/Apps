c370922cf19e5269057df30e315fa318008c736a
Apoorva Tare <apoorvatare@gmail.com>
2013-09-15 19:26:57 -0700

    Added link_to to all the views, also added db_migrate

--- scrum-int/db/migrate/20130915202229_create_teams.rb
+++ scrum-int/db/migrate/20130915202229_create_teams.rb
@@ -2,7 +2,7 @@ class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
       t.string :team_name
-      t.int :member_id
+      t.string :member_id
 
       t.timestamps
     end
