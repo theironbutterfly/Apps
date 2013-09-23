6b0d492b2aa33e5ebca52cb1b15b62ae999dd84c
Apoorva Tare <apoorvatare@gmail.com>
2013-09-23 09:41:57 -0700

    Added member model spec

--- scrum-int/spec/models/member_spec.rb
+++ scrum-int/spec/models/member_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Member do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can successfully add a member to a team' do
+    team = Team.new
+    team.members.name = "apoorva"
+    team.members.email = "ap@gmail.com"
+    team.member.save
+    expect(Team.member.find(1)).eq(team.members)   
+  end
 end
